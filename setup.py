63adbf9b460a5330ffd73e6eabbe7c2a37e08800
X2Super <102740303+x2super@users.noreply.github.com>
2025-04-14 19:27:31 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/x2super/kasawa",
-    author="Kasawa",
+    author="x2super",
     author_email="kasawaonecap@gmail.com",
     license="MIT",
     classifiers=[
