0e5065aa1417621db2c15eac7e00dd064ae99941
unknown <your@email.com>
2025-04-14 19:15:04 +0700

    0.0.2 version

---
+++ app/kasawa/__init__.py
@@ -0,0 +1,4 @@
+from .version import __version__
+from .topup import topup
+
+__all__ = ["topup"]
---
+++ app/kasawa/topup.py
@@ -0,0 +1,15 @@
+import cloudscraper
+
+def topup(phone, vouch):
+    """
+    This function redeems TrueMoney Wallet voucher by phone number and code.
+    Uses Cloudscraper to bypass Cloudflare.
+    """
+    scraper = cloudscraper.create_scraper()
+    payload = {
+        "phone": phone,
+        "vouch": vouch
+    }
+
+    r = scraper.post("https://api.kasawa.pro/api/wallet/topup", json=payload)
+    return r.json()
